5e130826b06f6c4a119f83270138a7c908a6e995
Vinayak Shinde <vinayakshnd@gmail.com>
2017-10-18 06:04:18 -0400

    Added support for CentOS 7 instance provision AWS

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM python:2.7
 
 RUN mkdir -p /root/px_prov /root/.ssh && cd /root/px_prov
-ADD https://releases.hashicorp.com/terraform/0.9.11/terraform_0.9.11_linux_amd64.zip /usr/bin
+ADD https://releases.hashicorp.com/terraform/0.10.7/terraform_0.10.7_linux_amd64.zip /usr/bin
 RUN apt-get update && apt-get install unzip
-RUN cd /usr/bin && unzip terraform_0.9.11_linux_amd64.zip && chmod 755 terraform
+RUN cd /usr/bin && unzip terraform_0.10.7_linux_amd64.zip && chmod 755 terraform
 COPY keys/id* /root/.ssh/
 COPY . /root/px_prov/
 WORKDIR /root/px_prov
@@ -14,6 +14,9 @@ ENV TF_VAR_azure_subscription_id "_AZURE_SUB_ID_"
 ENV TF_VAR_azure_client_id "_AZURE_CLIENT_ID_"
 ENV TF_VAR_azure_client_secret "_AZURE_CLIENT_SECRET_"
 ENV TF_VAR_azure_tenant_id "_AZURE_TENANT_ID_"
+ENV TF_VAR_aws_access_key "__AWS_ACCESS_KEY__"
+ENV TF_VAR_aws_secret_key "__AWS_SECRET_KEY__"
+ENV TF_VAR_aws_key "__AWS_KEY_NAME__"
 ENV GCP_PROJECT '_GCP_PROJECT_ID_'
 ENV GCP_SA_JSON '_CONTENTS_OF_GCP_SERVICE_ACCOUNT_JSON_FILE_'
 CMD python px_provision.py
---
+++ aws_functions.py
@@ -0,0 +1,63 @@
+import json
+import sys
+import subprocess
+import paramiko
+from px_functions import install_px
+
+#
+# Terraform will create VM, attach disks and also install docker via provisioning
+# All that we need to do is
+# 1. Generate output JSON
+# 2. Run PX installation script and check health of px installation
+
+
+def gen_aws_json(user_prefix, inst_px):
+    """
+    Function to generate output json as well as install portworx on each VM created with prefix defined by user_prefix
+    :param user_prefix: string
+    :return: None
+    """
+    tf_out = json.loads(subprocess.check_output('terraform output -no-color -json -state output/aws_{}.tfstate'
+                                                .format(user_prefix), shell=True))
+    pub_ip_list = tf_out['public_ips']['value']
+    admuser = tf_out['admuser']['value']
+    admpass = tf_out['admpassword']['value']
+    private_ip_list = tf_out['private_ips']['value']
+    json_out = []
+    count = 0
+    for public_ip in pub_ip_list:
+        docker_disk = '/dev/xvdc'
+        other_disks = []
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(public_ip, username=admuser, password=admpass)
+        stdin, stdout, stderr = ssh.exec_command("lsblk")
+        outlist = stdout.readlines()
+        for l in outlist:
+             if l.endswith('disk \n') and l.split()[0] not in ['xvda', 'xvdcz', 'xvdc']:
+                other_disks.append("/dev/{}".format(l.split()[0]))
+        instance_details = {"User": admuser,
+                        "Passwd": admpass,
+                        "PublicIpAddress": public_ip,
+                        "PrivateIpAddress": private_ip_list[count],
+                        "Port": "22",
+                        "DockerDisk": docker_disk,
+                        "Disks": other_disks}
+        json_out.append(instance_details)
+        count = count + 1
+        if inst_px:
+            px_installed, px_msg = install_px(public_ip, admuser, admpass)
+            if px_installed:
+                print "INFO : Portworx installed successfully on {}".format(public_ip)
+            else:
+                print "ERROR : Portworx installation failed on {} with following error message: {}". \
+                    format(public_ip, px_msg)
+    with open('output/aws_{}_output.json'.format(user_prefix), mode='w') as outfile:
+        outfile.write(json.dumps(json_out, indent=4))
+    print "========================================================"
+    print " Output JSON is at output/aws_{}_output.json".format(user_prefix)
+    print "========================================================"
+
+
+if __name__ == '__main__':
+    gen_aws_json(sys.argv[1], True)
